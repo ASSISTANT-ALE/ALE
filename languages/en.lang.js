59cbbae829dbd02332bb849b0164736977cf7773
/ A N K I T / <116625135+xnkit69@users.noreply.github.com>
2025-06-18 03:41:37 +0530

    Update en.lang.js

--- languages/en.lang.js
+++ languages/en.lang.js
@@ -3,10 +3,10 @@ module.exports = {
   helpHint: "Agar confuse ho gaye ho toh yaha message krdi ☞︎︎︎@xnkit69☜︎︎︎ main bata dunga!",
   commandError: "Oh nooo! Kuch toh galat ho gaya... Type `/x on` to start AI",
   startMessage: (name, botname) =>
-    `Hii ${name}!\n\n🌹𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽🇮🇳\n\n☄️𝗕𝗢𝗧 𝗡𝗔𝗠𝗘☄️ ⚔ ${botname} ⚔\n\nDeveloper ☞︎︎︎ Ankit Kumar☜︎︎︎✰\n\n
-🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂➪\n
-Telegram 👉https://t.me/xnkit69\n
-Instagram 👉 https://instagram.com/aannkkittttt\n
-Email 👉 xnkitk69@gmail.com\n
+    `Hii ${name}!\n\n🌹𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽🇮🇳\n☄️𝗡𝗔𝗠𝗘☄️ ⚔ ${botname} ⚔\nDeveloper ☞︎︎︎ Ankit Kumar☜︎︎︎✰\n\n
+🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂➪
+Telegram 👉https://t.me/xnkit69 | @xnkit69
+Instagram 👉 https://instagram.com/aannkkittttt
+Email 👉 xnkitk69@gmail.com
 Website 👉 https://xnkit69.netlify.app`,
 };
