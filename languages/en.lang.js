2be0660738355b1656dfa2ceca1d093bf69de0f3
/ A N K I T / <116625135+xnkit69@users.noreply.github.com>
2025-06-18 01:39:48 +0530

    Add files via upload

---
+++ languages/en.lang.js
@@ -0,0 +1,7 @@
+module.exports = {
+  unknownCommand: "Umm...? Yeh kya tha? Mujhe toh samajh hi nahi aaya!",
+  helpHint: "Agar confuse ho gaye ho toh /help type karo na, main bata dunga!",
+  commandError: "Oh nooo! Kuch toh galat ho gaya... thoda der baad try kar lena please?",
+  startMessage: (name, botname, prefix) =>
+    `Hii ${name}!\nWelcome to *${botname}* 🥳🥳🥳!\nAgar kuch chahiye toh just type \`${prefix}help\`, okayy?`,
+};
